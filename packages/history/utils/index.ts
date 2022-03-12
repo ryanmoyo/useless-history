099602864c08b9aad584558c460176d053efe60d
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-12 18:45:45 +0800

    🏗️ dir changes

--- packages/history/utils/index.ts
+++
@@ -1,6 +0,0 @@
-import { To } from '../types'
-
-export const createKey = () => Math.random().toString(36).substr(2, 8)
-
-export const createPath = (to: To) =>
-  typeof to === 'string' ? to : `/${to.pathname}?${to.search}#${to.hash}`
